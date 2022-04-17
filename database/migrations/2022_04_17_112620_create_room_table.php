7bfafc3d02e8c89822f1e0e55167a935821fb922
Sempurna 1 <profesorrob01@gmail.com>
2022-04-17 18:34:50 +0700

    Membuat Migration table meet dan room

---
+++ database/migrations/2022_04_17_112620_create_room_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->datetime('datetime');
+            $table->text('description');
+            $table->boolean('isOnline');
+            $table->boolean('isBooked')->default(false);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
