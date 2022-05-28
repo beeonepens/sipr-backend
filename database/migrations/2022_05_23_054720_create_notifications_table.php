c0426b93a58a5862c66f7a873224a3d508703f31
Sempurna 1 <profesorrob01@gmail.com>
2022-05-28 13:13:49 +0700

    first commit

---
+++ database/migrations/2022_05_23_054720_create_notifications_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('isRead')->default(false);
+            $table->enum('notificationType', ['notification', 'invitation']);
+            $table->date('publicationDate');
+            $table->timestamps();
+
+            $table->unsignedInteger('meet_id');
+            $table->foreign('meet_id')
+                ->references('id_meet')
+                ->on('meet')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');;
+
+            $table->string('user_id');
+            $table->foreign('user_id')
+                ->references('nip')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
